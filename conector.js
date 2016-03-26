e88c22378c23a0467b46bc2064a2b5ca5fc56c3d
MihajloDjokic <djmihajlo04@gmail.com>
2016-03-26 11:10:55 +0100

    Added files via upload

---
+++ conector.js
@@ -0,0 +1,25 @@
+
+
+    function bb() {
+		
+		var clicks= prompt("Broj likova")
+		
+         if(clicks==1){
+		     window.location="1.html"
+			 }
+	     if(clicks==2){
+			 window.location="2.html";
+			 }
+         if(clicks==3){
+		     window.location="3.html";			 
+		     }
+         if(clicks==4) {
+		     window.location="4.html";
+		     }		 
+         if(clicks>4) {
+		     window.location="4.html";
+		     }
+       
+   }
+   
+    
